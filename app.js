6b36bcf7eecf3bbd5c93bac4d9f93de89590a79e
Fenner Eduardo González Castellanos <fennereduardo@gmail.com>
2022-04-27 17:26:05 -0500

    :art: Servering a new template in public folder, :fire: Setting routes in app.js

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ const port = 8080;
 // Serve static content
 app.use( express.static('public'));
 
-app.get('/hola-mundo', (req, res) => {
-    res.send('Hello World in its own route')
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+})
+app.get('/generic', (req, res) => {
+    res.sendFile(`${__dirname}/public/generic.html`)
+})
+
+app.get('/elements', (req, res) => {
+    res.sendFile(`${__dirname}/public/elements.html`)
 })
 
 app.get('*', (req, res) => {
