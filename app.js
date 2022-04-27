d3d011c309196238aa24652cb6423a896e8e5305
Fenner Eduardo González Castellanos <fennereduardo@gmail.com>
2022-04-27 16:39:25 -0500

    :fire: Creating a server using the http method from NodeJs, it is an example without to use any framework

---
+++ app.js
@@ -0,0 +1,29 @@
+const http = require('http')
+
+//create server on port 8080
+http.createServer((req, res) => {
+
+
+
+    // res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    res.writeHead(200, { 'Content-Type': 'application/csv' })
+
+    // const persona = {
+    //     id: 1,
+    //     name: 'Fenner'
+    // }
+
+    // writting an response
+    // res.write(JSON.stringify(persona));
+    res.write('id, nombre\n');
+    res.write('1, Fenner\n');
+    res.write('2, Lina\n');
+    res.write('3, Liam\n');
+    res.write('4, Marlen\n');
+    res.write('5, Jorge\n');
+    res.end();
+})
+    .listen(8080)
+
+console.log('Escuchando el puerto', 8080);
